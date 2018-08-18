7129a3b8da247040f2543e514587c11084d55a3a
1843080373 <1843080373@qq.com>
2018-08-18 10:55:50 +0800

    推荐

--- src/main/java/com/zimadai/boot/BootApplication.java
+++ src/main/java/com/zimadai/boot/BootApplication.java
@@ -1,14 +1,17 @@
 package com.zimadai.boot;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.zimadai.boot.mapper")//将项目中对应的mapper类的路径加进来就可以了
-public class BootApplication {
+public class BootApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(BootApplication.class);
+    }
 }
